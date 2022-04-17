e5461819b7c36015622d37821c9dc239ca8cc522
Filipe Ferreira <jfilipeferreira@ua.pt>
2022-04-17 17:19:10 +0100

    cleaned some code

--- src/components/Scroll.js
+++ src/components/Scroll.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const Scroll = (props) => {
-    console.log(props.children);
+    
     return (
         <div style={{overflowY: 'scroll', borderTop: '2px solid black', overflow: 'hidden'}}>
             {props.children}
