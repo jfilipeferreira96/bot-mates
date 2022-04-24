604dedfc12dc0976cb5eff6f00890b92e695db93
Filipe Ferreira <jfilipeferreira@ua.pt>
2022-04-25 00:36:35 +0100

    uptaded to the earliest react syntax

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,72 +1,51 @@
-import React, {Component} from 'react';
-import CardList from '../components/CardList';
-import SearchBox from '../components/SearchBox';
-import Scroll from '../components/Scroll';
-import './app.css' 
-    
-class App extends Component  {
-    constructor() {
-        super()
-        this.state = {
-            bots: [],
-            searchfield: ''
-        }
+import { useState, useEffect, useCallback } from "react";
+import CardList from "../components/CardList";
+import SearchBox from "../components/SearchBox";
+import Scroll from "../components/Scroll";
+import "./app.css";
+
+function App() {
+  const [bots, setBots] = useState([]);
+  const [search, setSearch] = useState("");
+
+  const fetchBots = useCallback(async () => {
+    try {
+      const response = await fetch(
+        "https://jsonplaceholder.typicode.com/users"
+      );
+      const bots = await response.json();
+      setBots(bots);
+    } catch (error) {
+      console.log(error);
     }
+  }, []);
 
-    /*componentDidMount() {
+  useEffect(() => {
+    fetchBots();
+  }, [fetchBots]);
 
-        fetch('https://jsonplaceholder.typicode.com/users')
-            .then(response => response.json())
-            .then(users => this.setState({ bots: users }))
-        
-    }*/
+  const filteredBots = bots.filter((bot) => {
+    return bot.name.toLowerCase().includes(search.toLowerCase());
+  });
 
-    async componentDidMount() {
-        try {
-            const response = await fetch(`https://jsonplaceholder.typicode.com/users`);
-            const json = await response.json();
-            this.setState({ bots: json });
-        } catch (error) {
-            console.log(error);
-        }
-    }
+  const onSearchChange = (e) => {
+    setSearch(e.target.value);
+  };
 
-
-    onSearchChange = (event) => {
-        this.setState({ searchfield: event.target.value });
-    }
-
-
-    render() {
-        //with out destructuring
-        /* const filteredBots = this.state.bots.filter(bot => {
-            return bot.name.toLowerCase().includes(this.state.searchfield.toLowerCase())
-        }) */
-
-        //with destructuring
-        const { bots, searchfield } = this.state;
-        const filteredBots = bots.filter(bot => {
-            return bot.name.toLowerCase().includes(searchfield.toLowerCase())
-        })
-
-        //if ajax takes too long
-        if (bots.length === 0) {
-            return <h1>Loading</h1>
-        } else {
-
-            return (
-                <div className="tc">
-                    <h1 className="f1">Bot Mates</h1>
-                    <SearchBox searchChange={this.onSearchChange} />
-                    <Scroll>
-                        <CardList bots={filteredBots} />
-                    </Scroll>
-                </div>
-            ); 
-        }
-
-    }
-    
+  //if ajax takes too long
+  if (bots.length === 0) {
+    return <h1>Loading</h1>;
+  } else {
+    return (
+      <div className="tc">
+        <h1 className="f1">Bot Mates</h1>
+        <SearchBox onSearchChange={onSearchChange} />
+        <Scroll>
+          <CardList bots={filteredBots} />
+        </Scroll>
+      </div>
+    );
+  }
 }
 
 export default App;
